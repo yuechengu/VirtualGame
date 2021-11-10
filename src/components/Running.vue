ab93d9b4fa06263c22f35dbf0626fb7ceecaef79
yuechengu <yuechen.gu@accenture.com>
2021-11-10 18:31:41 +0900

    11/10

--- src/components/Running.vue
+++ src/components/Running.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="running">
-    <h1>请选择运动员和地图</h1><br>
+    <h1>请选择运动员和地图</h1>
+    <br />
+    <!-- 这里要改key,对应的是每个运动员的id -->
     <el-select v-model="player" multiple placeholder="请选择选手">
       <el-option
         v-for="item in playerOptions"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
+        :key="item.name"
+        :label="item.name"
+        :value="item.name"
       >
       </el-option>
     </el-select>
@@ -25,9 +27,9 @@
       >
       </el-option>
     </el-select>
-    <br>
-    <br>
-    <br>
+    <br />
+    <br />
+    <br />
     <el-button type="primary" @click="startGame()">开始比赛</el-button>
   </div>
 </template>
@@ -36,11 +38,7 @@
 export default {
   data() {
     return {
-      playerOptions: [
-        { key: "1", label: "张三" },
-        { key: "2", label: "李四" },
-        { key: "3", label: "王五" },
-      ],
+      playerOptions: [],
       mapOptions: [
         { key: "1", label: "图1" },
         { key: "2", label: "图2" },
@@ -54,14 +52,21 @@ export default {
       },
     };
   },
-    
+
   methods: {
+    //加载注册运动员
+    fetchRunners() {
+      this.$http.get("http://localhost:3000/runners").then(function (response) {
+        console.log("This request is succeed! Here is the response:");
+        this.playerOptions = response.body;
+      });
+    },
     //开始比赛
     startGame() {
       let form = null;
       form = this.nextGameForm;
       const params = form;
-      const res = this.startGame(params);//这地方有问题，要改
+      const res = this.startGame(params); //这地方有问题，要改
       console.log(res);
       if (res.code === "0000") {
         this.$message({
@@ -75,6 +80,9 @@ export default {
         message: "开始失败",
       });
     },
+  },
+  created() {
+    this.fetchRunners();
   },
 };
 </script>
