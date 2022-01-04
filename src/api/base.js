bc110008ee91515d8e9b9075245e3eca5310ab52
RanyeWzy <ranyewu@outlook.com>
2022-01-04 18:13:42 +0800

    1/4
    mock实时展示运动过程

--- src/api/base.js
+++ src/api/base.js
@@ -3,34 +3,34 @@
  */
 
 const base = {
-  //运动员管理（lsc提供）
-  searchAllPlayer: '/lishaochenService/player/queryplayer',
-  IDfindPlayer: '/lishaochenService/player/IDfindplayer',
-  insertPlayer: '/lishaochenService/player/insertplayer',
-  deletePlayer: '/lishaochenService/player/deleteplayer',
-  //地图管理（lsc提供）
-  searchAllMap: '/lishaochenService/mapandevent/queryallmap',
-  //比赛结果管理（lsc提供）
-  searchGameResult: '/lishaochenService/gameresult/queryresult',
-  //比赛（lx提供）
-  running: '/lixiService/sports/running',//NG：Bad Request, 把get改成post应该就行了
+  // //运动员管理（lsc提供）
+  // searchAllPlayer: '/lishaochenService/player/queryplayer',
+  // IDfindPlayer: '/lishaochenService/player/IDfindplayer',
+  // insertPlayer: '/lishaochenService/player/insertplayer',
+  // deletePlayer: '/lishaochenService/player/deleteplayer',
+  // //地图管理（lsc提供）
+  // searchAllMap: '/lishaochenService/mapandevent/queryallmap',
+  // //比赛结果管理（lsc提供）
+  // searchGameResult: '/lishaochenService/gameresult/queryresult',
+  // //比赛（lx提供）
+  // running: '/lixiService/sports/running',//NG：Bad Request, 把get改成post应该就行了
 
   /**
    * 前端mockService模拟后端接口
    * 工具：json-server
    * 端口：3000
    */
-  // //运动员管理
-  // searchAllPlayer: '/mockService/player',
-  // IDfindPlayer: '',
-  // insertPlayer: '',
-  // deletePlayer: '',
-  // //地图管理
-  // searchAllMap: '/mockService/map',
-  // //比赛结果管理
-  // searchGameResult: '/mockService/playerRecord',
-  // //比赛
-  // running: '',  
+  //运动员管理
+  searchAllPlayer: '/mockService/player',
+  IDfindPlayer: '',
+  insertPlayer: '',
+  deletePlayer: '',
+  //地图管理
+  searchAllMap: '/mockService/map',
+  //比赛结果管理
+  searchGameResult: '/mockService/playerRecord',
+  //比赛
+  running: '',  
 };
 
 export default base;
--- src/pages/Running.vue
+++ src/pages/Running.vue
@@ -45,15 +45,57 @@
     <el-button type="info" @click="backToMain()">返回</el-button>
 
     <!-- el-dialog 是弹窗样式，title 绑定弹窗的标题内容，visible 绑定弹窗是否展示 -->
-    <el-dialog title="比赛结果" :visible.sync="dialogVisible">
+    <el-dialog :title="dialogtitle" :visible.sync="dialogVisible">
+      <el-divider>显示比赛过程: {{progressSec}}秒</el-divider>
+      <el-row>
+        <el-col :span="2">
+          <i class="el-icon-user-solid" >{{player1Details.playerName}}</i>
+        </el-col>
+        <el-col :span="10">
+          <el-progress :percentage="player1Details.processPer" color="#8e71c7"></el-progress>
+        </el-col>
+        <el-col :span="12" align="left">
+          <i class="el-icon-chat-line-square">{{player1Details.processMsg}}</i>
+        </el-col>
+      </el-row>
+      <br />
+      <el-row>
+        <el-col :span="2">
+          <i class="el-icon-user-solid" >{{player2Details.playerName}}</i>
+        </el-col>
+        <el-col :span="10">
+          <el-progress :percentage="player2Details.processPer" color="#6f7ad3"></el-progress>
+        </el-col>
+        <el-col :span="12" align="left">
+          <i class="el-icon-chat-line-square">{{player2Details.processMsg}}</i>
+        </el-col>
+      </el-row>
+      <br />
+      <el-row>
+        <el-col :span="2">
+          <i class="el-icon-user-solid" >{{player3Details.playerName}}</i>
+        </el-col> 
+        <el-col :span="10">
+          <el-progress :percentage="player3Details.processPer" color="#6f7ad3"></el-progress>
+        </el-col>
+        <el-col :span="12" align="left">
+          <i class="el-icon-chat-line-square">{{player3Details.processMsg}}</i>
+        </el-col>
+      </el-row>
+      <br />
+      <br /><br />
+      <el-button type="primary" @click="clickBtn()" :disabled="processDisplay">显示</el-button>
+
+      <br />
+      <el-divider>显示比赛结果</el-divider>
       <el-table
         :data="records"
-        v-loading="loading"
+        v-loading="tableLoading"
         element-loading-text="比赛进行中，请稍等片刻~"
         element-loading-spinner="el-icon-loading"
         style="width: 100%"
       >
-        <el-table-column prop="playerId" label="参赛人ID"> </el-table-column>
+        <el-table-column prop="playerName" label="参赛人姓名"> </el-table-column>
         <el-table-column prop="league" label="排名"> </el-table-column>
         <el-table-column prop="gameSpeed" label="比赛中速度"> </el-table-column>
         <el-table-column prop="gameCommentary" label="比赛描述">
@@ -83,9 +125,31 @@ export default {
       //弹窗是否出现
       dialogVisible: false,
       //是否加载遮罩
-      loading: false,
+      tableLoading: true,
+      //显示按钮是否可用
+      processDisplay: false,
+      //过程初始时间
+      progressSec : 0,
+      //各运动员初始进度和文本
+      player1Details: {
+          playerName : "张三",
+          processPer : 0,
+          processMsg : "开始",
+      },
+      player2Details: {
+          playerName : "李四",
+          processPer : 0,
+          processMsg : "开始",
+      },
+      player3Details: {
+          playerName : "王五",
+          processPer : 0,
+          processMsg : "开始",
+      },
       //结果表
-      records: [],      
+      records: [],
+      dialogtitle: "比赛结果"
+      //参赛细节表
     };
   },
 
@@ -105,16 +169,17 @@ export default {
         //控制台看一下提交的表单
         console.log(JSON.stringify(this.form));
         //发出post请求
-        this.$api.gameService.startBy(this.form)
-          .then((result) => {
-            console.log("This request succeeded! Here is the response of running:");          
-            //控制台看一下返回的表单
-            console.log(result.data);
-            //返回比赛结果表，并提供下载
-            this.records = response.body;
-          })        
-      };
-    },  
+        this.$api.gameService.startBy(this.form).then((result) => {
+          console.log(
+            "This request succeeded! Here is the response of running:"
+          );
+          //控制台看一下返回的表单
+          console.log(result.data);
+          //返回比赛结果表，并提供下载
+          this.records = response.body;
+        });
+      }
+    },
     backToMain() {
       this.$router.push("/");
     },
@@ -123,20 +188,59 @@ export default {
     },
     //重置方法
     resetRunning() {
-      this.form.players=[];
-      this.form.mapName="";
+      this.form.players = [];
+      this.form.mapName = "";
     },
     //加载运动员
     fetchplayers() {
       this.$api.playerService.playerList().then((result) => {
         this.playerOptions = result.data;
-      });;
+      });
     },
     //加载地图
     fetchMaps() {
       this.$api.mapService.mapList().then((result) => {
         this.mapOptions = result.data;
       });
+    },
+    //获取运动员1的运动细节
+    fetchPlayer1Process(processSec){
+      this.$http.get("http://localhost:3000/playerProDetails?playerName=%E5%BC%A0%E4%B8%89&processSec="+processSec)
+          .then(function(response){
+            this.player1Details = response.body[0];
+          });
+      this.$http.get("http://localhost:3000/playerProDetails?playerName=%E6%9D%8E%E5%9B%9B&processSec="+processSec)
+          .then(function(response){
+            this.player2Details = response.body[0];
+          });
+      this.$http.get("http://localhost:3000/playerProDetails?playerName=%E7%8E%8B%E4%BA%94&processSec="+processSec)
+          .then(function(response){
+            this.player3Details = response.body[0];
+          });
+    },
+    clickBtn() {
+      //定义定时器开始时间为0
+      var progressSecTemp = 0;
+      // //禁用二次点击
+      this.processDisplay = true;
+      //定一个定时器
+      var timer = setInterval(() => {
+        //变量一直++
+        progressSecTemp++;
+        this.fetchPlayer1Process(progressSecTemp);
+        //清楚定时器
+        if (progressSecTemp >= 50) {
+          this.$http.get("http://localhost:3000/mockresult")
+          .then(function(response){
+            this.records = response.body;
+          });
+          this.tableLoading = false; 
+          clearInterval(timer);
+        }
+        //获取重新赋值
+        this.progressSec = progressSecTemp;
+
+      }, 1000);
     },
   },
   created() {
